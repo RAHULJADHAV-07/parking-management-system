3bede6e44064f706c659f079d2f3ae65146f9cfc
CR <vu4f2223004@pvppcoe.ac.in>
2025-04-28 12:25:33 +0530

    feat: Implement vehicle service with CRUD operations and records management

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://parking-backend.onrender.com/api'
+  apiUrl: 'https://parking-management-system-9sqf.onrender.com/api/vehicles/all-vehicles'
 };
 
 export const environment_dev = {
